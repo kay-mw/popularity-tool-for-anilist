a4abf122d44441e012c831b196b977603ede05ac
kay-mw <kiranwhalleymalhotra@gmail.com>
2024-02-29 21:56:44 +0000

    Removed unnecessary socketIO code in app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 
 def process_data(anilist_id):
@@ -14,9 +12,7 @@ def process_data(anilist_id):
 def anilist():
     if request.method == 'POST':
         anilist_id = request.form.get('anilist_id')
-        socketio.emit('start_processing', namespace='/loader')
         process_data(anilist_id)
-        socketio.emit('stop_processing', namespace='/loader')
         return redirect(url_for('dashboard'))
     return render_template('home.html')
 
@@ -27,4 +23,4 @@ def dashboard():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
+    app.run(host='0.0.0.0', debug=True)
