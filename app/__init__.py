5fcc472efa51e2de86dd0002340e09b8ef793cd2
kay-mw <kiranwhalleymalhotra@gmail.com>
2024-03-13 16:46:28 +0000

    Added app startup file to setup waitress production server

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,3 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(main)
     return app
-
-
-# mode = "dev"
-
-# if __name__ == '__main__':
-#     if mode == "dev":
-#         main.run(host='0.0.0.0', debug=False)
-#     else:
-#         serve(main)
