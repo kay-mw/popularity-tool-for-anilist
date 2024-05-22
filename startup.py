6a064d01e6621d98c00334d0fd52478e1e83c102
kay-mw <kiranwhalleymalhotra@gmail.com>
2024-05-22 16:47:42 +0100

    Updated waitress serve to use https url_scheme

--- startup.py
+++ startup.py
@@ -5,4 +5,4 @@ app = create_app()
 wsgi_app = app.wsgi_app
 
 if __name__ == "__main__":
-    serve(wsgi_app, host='0.0.0.0', port=8000)
+    serve(wsgi_app, host='0.0.0.0', port=8000, url_scheme='https')
