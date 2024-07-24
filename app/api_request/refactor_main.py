a7ca188ee77eab5031c526f00b56bb4ae35d81b2
kay-mw <kiranwhalleymalhotra@gmail.com>
2024-07-24 11:30:11 +0100

    Improved code quality of main API script, fixed undefined errors, added type hints

--- app/api_request/refactor_main.py
+++ app/api_request/refactor_main.py
@@ -6,72 +6,47 @@ def fetch_data(username: str):
     import pandas as pd
     import requests
 
-    def load_query(file_name):
+    def load_query(file_name: str) -> str:
         file_path = os.path.join("./app/api_request/", file_name)
         with open(file_path, "r") as file:
             return file.read()
 
-    def fetch_anilist_data(query, variables):
-        try:
-            response = requests.post(
-                url, json={"query": query, "variables": variables}, timeout=10
-            )
-            response.raise_for_status()
-            global response_header
-            response_header = response.headers["Date"]
-            response_header = pd.Series(response_header)
-            return response.json()
-        except requests.exceptions.Timeout:
-            print("Request timed out.")
-            return None
-        except requests.exceptions.RequestException as e:
-            print(f"Error in request: {e}")
-            return None
-
-    async def fetch_anilist_data_async(query, variables):
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(
-                    url, json={"query": query, "variables": variables}, timeout=10
-                ) as response:
-                    response.raise_for_status()
-                    global response_header
-                    response_header = response.headers["Date"]
-                    response_header = pd.Series(response_header)
-                    return await response.json()
-        except asyncio.TimeoutError:
-            print("Request timed out.")
-            return None
-        except aiohttp.ClientResponseError as e:
-            print(f"Error in request: {e}")
-            return None
-
-    query_get_id = load_query("get_id.gql")
-
-    variables_get_id = {"name": username}
-
     url = "https://graphql.anilist.co"
 
-    json_response = fetch_anilist_data(query_get_id, variables_get_id)
+    def fetch_anilist_data(query: str, variables: dict) -> tuple[dict, pd.Series]:
+        response = requests.post(
+            url, json={"query": query, "variables": variables}, timeout=10
+        )
+        response.raise_for_status()
+        response_header = pd.Series(response.headers["Date"])
+        return response.json(), response_header
+
+    async def fetch_anilist_data_async(query: str, variables: dict) -> dict:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url, json={"query": query, "variables": variables}, timeout=10
+            ) as response:
+                response.raise_for_status()
+                return await response.json()
+
+    query_get_id = load_query("get_id.gql")
+    variables_get_id = {"name": username}
+    json_response, response_header = fetch_anilist_data(query_get_id, variables_get_id)
     anilist_id = json_response["data"]["User"]["id"]
 
-    # # # # # # # # # # # # # # # # # # # # #
+    # NOTE: Fetch user scores
 
     query_user = load_query("user_query.gql")
-
     variables_user = {"page": 1, "id": anilist_id}
-
-    json_response = fetch_anilist_data(query_user, variables_user)
+    json_response, response_header = fetch_anilist_data(query_user, variables_user)
 
     user_score = pd.json_normalize(
         json_response,
         record_path=["data", "Page", "users", "statistics", "anime", "scores"],
         meta=[["data", "Page", "users", "id"]],
     )
-
     user_score = user_score.explode("mediaIds", ignore_index=True)
     user_score["mediaIds"] = user_score["mediaIds"].astype(int)
-
     user_score.rename(
         columns={
             "mediaIds": "anime_id",
@@ -87,35 +62,30 @@ def fetch_data(username: str):
     else:
         pass
 
-    # # # # # # # # # # # # # # # # # # # # # # # # Make user info table # # # # # # # # # # # # # # # # # # # # #
+    # NOTE: Make user info table
 
     user_info = pd.json_normalize(json_response, record_path=["data", "Page", "users"])
     user_info.drop("statistics.anime.scores", axis=1, inplace=True)
-
     user_info = pd.concat([user_info, response_header], axis=1)
     user_info.rename(
         columns={0: "request_date", "id": "user_id", "name": "user_name"},
         inplace=True,
     )
-
     user_info["request_date"] = pd.to_datetime(
         user_info["request_date"], format="%a, %d %b %Y %H:%M:%S %Z"
     ).dt.tz_localize(None)
 
-    # # # # # # # # # # # # # # # # # # # # # # # # # Get anime info # # # # # # # # # # # # # # # # # # # # # # #
+    # NOTE: Get anime info
 
     async def main():
         anime_info = pd.DataFrame()
 
         id_list = user_score["anime_id"].values.tolist()
-
         variables_anime = {"page": 1, "id_in": id_list}
-
         query_anime = load_query("anime_query.gql")
 
         while True:
             response_ids = await fetch_anilist_data_async(query_anime, variables_anime)
-
             page_df = pd.json_normalize(
                 response_ids, record_path=["data", "Page", "media"]
             )
@@ -139,15 +109,13 @@ def fetch_data(username: str):
         inplace=True,
     )
 
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # NOTE: Analysis
 
     merged_dfs = user_score.merge(anime_info, on="anime_id", how="left")
-
     merged_dfs["score_diff"] = merged_dfs["user_score"] - merged_dfs["average_score"]
 
     float_avg_score_diff = abs(merged_dfs.loc[:, "score_diff"]).mean()
     avg_score_diff = round(float_avg_score_diff, 2)
-
     float_true_score_diff = merged_dfs.loc[:, "score_diff"].mean()
     true_score_diff = round(float_true_score_diff, 2)
 
@@ -160,27 +128,33 @@ def fetch_data(username: str):
 
     score_max = int(max_diff["user_score"].iloc[0])
     score_min = int(min_diff["user_score"].iloc[0])
-
     avg_max = int(max_diff["average_score"].iloc[0])
     avg_min = int(min_diff["average_score"].iloc[0])
-
     title_max = max_diff["title_romaji"].iloc[0]
     title_min = min_diff["title_romaji"].iloc[0]
 
     image_id_1 = int(max_diff["anime_id"].iloc[0])
     image_id_2 = int(min_diff["anime_id"].iloc[0])
-
     query_image = load_query("image_query.gql")
-
     variables_image_1 = {"id": image_id_1}
     variables_image_2 = {"id": image_id_2}
-
-    cover_image_1 = fetch_anilist_data(query_image, variables_image_1)
-    cover_image_2 = fetch_anilist_data(query_image, variables_image_2)
-
+    cover_image_1, response_header = fetch_anilist_data(query_image, variables_image_1)
+    cover_image_2, response_header = fetch_anilist_data(query_image, variables_image_2)
     cover_image_1 = cover_image_1["data"]["Media"]["coverImage"]["extraLarge"]
     cover_image_2 = cover_image_2["data"]["Media"]["coverImage"]["extraLarge"]
 
-    # # # # # # # # # # # # # # # # # # # # # # # Data Quality Checks # # # # # # # # # # # # # # # # # # # # # # #
+    # NOTE: Return
 
-    return [anime_info, user_info, user_score]
+    analysis = (
+        avg_score_diff,
+        true_score_diff,
+        score_max,
+        score_min,
+        avg_max,
+        avg_min,
+        title_max,
+        title_min,
+    )
+    dfs = [anime_info, user_info, user_score]
+
+    return dfs, analysis
