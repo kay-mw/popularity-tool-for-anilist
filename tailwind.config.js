96e3feadd8e5718bedb9a973b95560f3a9ed78b5
kay-mw <kiranwhalleymalhotra@gmail.com>
2024-08-08 01:03:21 +0100

    Refactored home.html using DaisyUI classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,30 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00bbbc",
+
+          secondary: "#00c79c",
+
+          accent: "#c45a00",
+
+          neutral: "#1c1917",
+
+          "base-100": "#141414",
+
+          info: "#111827",
+
+          success: "#00ff81",
+
+          warning: "#ffd100",
+
+          error: "#ff84a5",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
