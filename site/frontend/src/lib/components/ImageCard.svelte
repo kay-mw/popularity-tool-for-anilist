878a0bcbb31c7436d5fb82d76d7cc76ff99d05ec
kay-mw <kiranwhalleymalhotra@gmail.com>
2024-10-06 21:34:07 +0100

    ui: improved score labels on ImageCard.svelte

--- site/frontend/src/lib/components/ImageCard.svelte
+++ site/frontend/src/lib/components/ImageCard.svelte
@@ -1,6 +1,7 @@
 <script>
 	import * as Card from "$lib/components/ui/card";
 	import H2 from "$lib/components/H2.svelte";
+	import { Separator } from "$lib/components/ui/separator";
 
 	export let title = "";
 	export let description = "";
@@ -17,23 +18,31 @@
 		<Card.Title class={textColour}>{title}</Card.Title>
 		<Card.Description>{description}</Card.Description>
 	</Card.Header>
-	<Card.Content class="flex flex-col justify-center space-y-4 font-semibold">
+	<Card.Content class="relative justify-center space-y-4 font-semibold">
 		<H2 class="{textColour} text-2xl">{animeTitle}</H2>
-		<figure class="relative">
-			<img
-				class="flex border-solid border-2 border-border rounded-xl w-full"
-				src={image}
-				alt="coldest take anime"
-			/>
-			<div
-				class="absolute bottom-6 md:bottom-8 pt-1 md:pt-2 pl-3 h-[3rem] md:h-[4.5rem] bg-secondary w-full drop-shadow-lg"
-				style="--tw-bg-opacity: 0.85; background-color: hsl(var(--secondary) / var(--tw-bg-opacity));"
-			>
-				<div class="font-semibold text-sm md:text-lg">
-					<p class={textColour}>{username}: {userScore}</p>
-					<p class="text-plot-accent">AniList: {avgScore}</p>
-				</div>
+		<div class="relative group">
+			<figure>
+				<img
+					class="border-solid border-2 border-border rounded-lg w-full opacity-20 md:opacity-100 group-hover:opacity-20 md:group-hover:opacity-20 transition-opacity duration-500 ease-in-out"
+					src={image}
+					alt="coldest take anime"
+				/>
+			</figure>
+			<div class="absolute space-y-2 font-semibold text-xl md:text-3xl md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-500 ease-in-out top-1/2 left-1/2" style="transform: translate(-50%, -50%);">
+				{#if userScore < avgScore}
+					<p class={textColour}>{username} ↓ {userScore}</p>
+					<Separator />
+					<p class="text-plot-accent">AniList ↑ {avgScore}</p>
+				{:else if userScore == avgScore}
+					<p class={textColour}>{username} ~ {userScore}</p>
+					<Separator />
+					<p class="text-plot-accent">AniList ~ {avgScore}</p>
+				{:else}
+					<p class={textColour}>{username} ↑ {userScore}</p>
+					<Separator />
+					<p class="text-plot-accent">AniList ↓ {avgScore}</p>
+				{/if}
 			</div>
-		</figure>
+		</div>
 	</Card.Content>
 </Card.Root>
