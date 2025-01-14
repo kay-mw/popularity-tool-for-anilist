505945416ffeaa86142f45111cf6bdcace3a1ecc
kay-mw <kiranwhalleymalhotra@gmail.com>
2025-01-14 16:57:49 +0000

    ui: added entry animation

--- site/frontend/src/lib/components/AnimatedScroll.svelte
+++ site/frontend/src/lib/components/AnimatedScroll.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
-	let { threshold = 0.2, children, duration = "2s" } = $props();
+	let {
+		threshold = 0.2,
+		movement = "20px",
+		fadeInTime = 0.4,
+		opacityDuration = "2s",
+		transformDuration = "2s",
+		delay = "0s",
+		children,
+		class: className = "",
+	} = $props();
 
 	let element: HTMLDivElement;
 	let intersecting = $state();
@@ -21,7 +30,12 @@
 <div
 	bind:this={element}
 	class:animated={intersecting}
-	style="--duration: {duration}"
+	style="--opacity-duration: {opacityDuration}; 
+	--transform-duration: {transformDuration}; 
+	--movement: {movement}; 
+	--fade-in-time: {fadeInTime}; 
+	--delay: {delay};"
+	class={className}
 >
 	{@render children?.()}
 </div>
@@ -29,10 +43,13 @@
 <style>
 	div {
 		opacity: 0;
-		transform: translateY(20px);
+		transform: translateY(var(--movement));
 		transition:
-			opacity var(--duration),
-			transform 2s;
+			opacity var(--opacity-duration)
+				cubic-bezier(var(--fade-in-time), 0, 0.2, 1),
+			transform var(--transform-duration)
+				cubic-bezier(var(--fade-in-time), 0, 0.2, 1);
+		transition-delay: var(--delay);
 	}
 
 	div.animated {
