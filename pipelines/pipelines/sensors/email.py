245b64c6ace2f4aa55fc4d67c25fea5e79a6d3b1
kay-mw <kiranwhalleymalhotra@gmail.com>
2025-01-14 15:26:05 +0000

    pipeline: enable email sensor by default

--- pipelines/pipelines/sensors/email.py
+++ pipelines/pipelines/sensors/email.py
@@ -1,9 +1,10 @@
 import os
 
-from dagster import make_email_on_run_failure_sensor
+from dagster import DefaultSensorStatus, make_email_on_run_failure_sensor
 
 email_on_run_failure = make_email_on_run_failure_sensor(
     email_from=os.environ["SENDER_EMAIL"],
     email_password=os.environ["APP_PASSWORD"],
     email_to=[os.environ["RECEIVER_EMAIL"]],
+    default_status=DefaultSensorStatus.RUNNING,
 )
